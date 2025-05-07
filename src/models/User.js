0785bc3e8c72e98eef3e0028ac6d677b7a929e1a
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-05-07 10:36:37 -0300

    feat: Adiciona validações para usuario

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,48 @@ import  Sequelize, { Model }  from "sequelize";
 export default class User extends Model {
     static init(sequelize) {
         super.init({
-            nome: Sequelize.STRING,
-            email: Sequelize.STRING,
-            password_hash: Sequelize.STRING,
-            password: Sequelize.VIRTUAL,
+            nome: { 
+               type: Sequelize.STRING,
+               defaultValue: '',
+               validate: {
+                len: {
+                    args: [3, 255],
+                    msg: 'Campo nome deve ter no minino 3 caracter',
+                },
+               },
+            },
+            email: {
+               type: Sequelize.STRING,
+               defaultValue: '',
+               validate: {
+                len: {
+                    args:[],
+                    msg: '',
+                },
+
+               },
+            },
+            password_hash: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    len: {
+                        args:[],
+                        msg: '',
+                    },
+                },
+            },
+            password: {
+               type: Sequelize.VIRTUAL,
+               defaultValue: '',
+               validate: {
+                len: {
+                    args: [],
+                    msg: '',
+                },
+               },
+
+            },
         },{
             sequelize,
         });
