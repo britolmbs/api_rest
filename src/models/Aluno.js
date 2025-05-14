2336a7b198e28cb6b1107424bf55e700a374f8cc
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-05-14 11:21:56 -0300

    feat: criação validação no Alino.js

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,12 +3,64 @@ import  Sequelize, { Model }  from "sequelize";
 export default class Aluno extends Model {
     static init(sequelize) {
         super.init({
-            nome: Sequelize.STRING,
-            sobrenome: Sequelize.STRING,
-            email: Sequelize.STRING,
-            idade: Sequelize.INTEGER,
-            peso: Sequelize.FLOAT,
-            altura: Sequelize.FLOAT,
+            nome: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                len: {
+                    args:[3, 255],
+                    msg: 'Nome necessario',
+                    },
+                },
+            },
+        
+            sobrenome: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    len: {
+                        args: [3, 25],
+                        msg: 'Sobrenome Necessario.',
+                    },
+                },
+            },
+            email: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    isEmail: {
+                        msg: 'e-mail invalido'
+                    },
+                },
+            },
+            idade: {
+                type: Sequelize.INTEGER,
+                defaultValue: '',
+                validate: {
+                    isInt:{
+                        msg: 'Idade Invalida',
+                    },
+                },
+
+            },
+            peso: {
+                type:Sequelize.FLOAT,
+                defaultValue: '',
+                validate: {
+                    isFloat: {
+                        msg: 'Peso Invalido',
+                    },
+                },
+            },
+            altura: {
+                type: Sequelize.FLOAT,
+                defaultValue: '',
+                validate: {
+                    isFloat:{ 
+                        msg: 'Altura Invalida',
+                    },
+                },
+            },
 
         }, {
             sequelize,
