cb78ad5bb590e9e50adca0b2bf99105718a6b5db
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-05-15 11:11:12 -0300

    feat: Criando o Read do CRUD de Alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,34 @@ class AlunoController {
         const alunos = await Aluno.findAll();
         res.json(alunos)
     }
+    async store(req, res){}
+
+    async update(req, res){}
+
+    async show(req, res){
+      try { 
+        const {id} = req.params;
+
+        if(!id) {
+            return res.status(404).json({
+                errors: ['Faltando ID'],
+            });
+        }
+        const aluno = await Aluno.findByPk(id);
+            if(!aluno) {
+                return res.status(404).json({
+                    errors: ['Aluno não existe']
+                });
+            }
+            return res.json(aluno);
+     }catch (e){
+        return res.status(400).json({
+            errors: e.errors.map((err) => err.message),
+        });
+
+     }
+    }
+    async delete(req, res){}
 }
 
 export default new AlunoController();
