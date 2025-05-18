a565c9ecdd704bfcdd5aaa229d0f7b6d510775e3
Lucas Brito <britolmbs@hotmail.com>
2025-05-18 08:56:53 -0300

    feat: Tratando os arquivos enviados

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,8 +1,15 @@
 import multer from "multer";
 import { extname, resolve } from 'path';
 
- const aleatorio = () => Math.floor(Math.random()* 10000 + 10000);
+
+ const aleatorio = () => Math.floor(Math.random() * 1000 + 1000);
 export default {
+    fileFilter: (req, file, cb) => {
+        if(file.mimetype != 'image/png' && file.mimetype != 'image/jpeg') {
+            return cb(new multer.MulterError('Arquivo precisa ser de imagem'));
+        }
+        return cb(null, true);
+    },
     stoage: multer.diskStorage({
         destination: (req, file, cb) => {
             cb(null, resolve(__dirname, '..', '..', 'uploads'));
