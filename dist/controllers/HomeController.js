dc33f5fcd9fae7f670ce96dcd48dd0e9a51d176e
Lucas Brito <britolmbs@hotmail.com>
2025-05-23 20:26:16 -0300

    criando a pasta de distribuição

---
+++ dist/controllers/HomeController.js
@@ -0,0 +1,9 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+
+class HomeControler {
+     async index(req, res) {
+        res.json('index');
+    }
+}
+
+exports. default = new HomeControler();
---
+++ dist/database/seeds/20250513001358-criar-usuarios.js
@@ -0,0 +1,42 @@
+"use strict";'use strict';
+const bcryptjs = require('bcryptjs');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+      await queryInterface.bulkInsert('users', [{
+       nome: 'Nicolas',
+       email: 'nicolas.s@teste.com.br',
+       password_hash: await bcryptjs.hash('123472', 8),
+       created_at: new Date(),
+       updated_at: new Date(),
+       
+      },
+    {
+      nome:'Olivia',
+      email: 'olivia.g@teste.com.br',
+      password_hash: await bcryptjs.hash('123473', 8),
+      created_at: new Date(),
+      updated_at:new Date(),
+    },
+  {
+    nome: 'Paulo',
+    email: 'paulo.h@teste.com.br',
+    password_hash: await bcryptjs.hash('123474', 8),
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    nome: 'Quiteria',
+    email: 'quiteria.j@teste.com.br',
+    password_hash: await bcryptjs.hash('123475', 8),
+    created_at: new Date(),
+    updated_at: new Date(),
+  }], {});
+    
+  },
+
+  async down () {
+  
+  }
+};
